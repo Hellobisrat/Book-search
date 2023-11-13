cb2750dfeb300314f74c20bec7f0e19bb91d477d
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2023-11-13 15:39:37 -0600

    some change

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -14,9 +14,9 @@ import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import {useMutation} from '@apollo/client'
 import { SAVE_BOOK } from '../utils/mutation';
 const SearchBooks = () => {
-  // create state for holding returned google api data
+  
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
+
   const [searchInput, setSearchInput] = useState('');
     const [saveBook ]= useMutation(SAVE_BOOK)
   // create state to hold saved bookId values
